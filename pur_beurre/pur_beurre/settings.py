8771524f46eaa3fb202c6595e3704b42ae0977cd
Julien-V <julien.vassort61@gmail.com>
2020-08-26 14:35:57 +0200

    SECRET_KEY in env var

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x9ypi2zqh=-!t6y4_e&t_&=b6+lx^%io1gh%ae%8_lf9(cr!%d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
